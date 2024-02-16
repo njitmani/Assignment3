e0561ec1ec5b7fa6e126e49ba8086a1bf21adfff
njitmani <mk2368@njit.edu>
2024-02-16 17:01:27 -0500

    calculator code modified into full developed stage

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -4,20 +4,20 @@ from calculator.operations import add, subtract, multiply, divide
 class Calculator:
   @staticmethod
   def add(a, b):
-    calculation =  Calculation(a, b, add)
-    return calculation.get_result()
+    calculation = Calculation(a, b, add)
+    return calculation.perform()
   
   @staticmethod
   def subtract(a, b):
-    calculation = Calculation(a, b, subtract)
-    return calculation.get_result()
+    calculation  = Calculation(a, b, subtract)
+    return calculation.perform()
   
   @staticmethod
   def multiply(a, b):
     calculation = Calculation(a, b, multiply)
-    return calculation.get_result()
+    return calculation.perform()
   
   @staticmethod
   def divide(a, b):
     calculation = Calculation(a, b, divide)
-    return calculation.get_result()
+    return calculation.perform()
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@
 
 This is the calculator code as part of assignment 3.
 created second branch part2 for modifications
+created a third branch part3 for modifications 
